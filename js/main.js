878ddf26e1d5e21242b7a19afafa874874b1cb87
FaSnow <eng.fbneves@gmail.com>
2022-09-07 22:01:25 -0300

    Add botão de Ordenar

--- js/main.js
+++ js/main.js
@@ -3,23 +3,75 @@ const divisaoResultados = document.querySelector('.blocoNumeros');
 const atalhos = document.querySelectorAll(".atalhos li");
 const ladosDado = document.getElementById('ladosDado');
 const nDados = document.getElementById('nDados');
+const botaoOrdenar = document.querySelector("#ordernarButton");
+
+let arrayNumeros = [];
+console.log(typeof arrayNumeros);
+
+// 
 
 for (let i = 0; i < atalhos.length; i++) {
     atalhos[i].onclick = function () {
         const textoAtalho = atalhos[i].textContent
-        textoAtalho.substring(1);
+        ladosDado.value = textoAtalho.substring(1);
     }
 }
 
-function generateDice() {   
+// 
+
+document.querySelector('.play').onclick = generateDice;
+
+botaoOrdenar.onclick = function () {
+    if (arrayNumeros == 0){
+        console.log("Sem Numeros");
+        publicar("null");
+    }
+    ordernamento(arrayNumeros);
+    publicar(arrayNumeros);
+}
+
+
+// Funções
+
+function generateDice() {
+    arrayNumeros = [];
     let resultHtml = "";
     for (let i = 0; i < nDados.value; i++) {
-        resultHtml += "<div class='apresentaDados'><p>" + String(Math.floor(Math.random() * ladosDado.value) + 1) + "</p></div>";
+        const RandNumero = Math.floor(Math.random() * ladosDado.value) + 1;
+        arrayNumeros.push(RandNumero);
+        resultHtml += padraoTexto(RandNumero);
     }
-    console.log(resultHtml);
+
+
     divisaoResultados.innerHTML = resultHtml;
 }
 
-document.querySelector('.play').onclick = generateDice;
+function publicar(conteudo) {
+
+    let textoPublicar = "";
+
+    for (let i = 0; i < conteudo.length; i++) {
+        const element = conteudo[i];
+        textoPublicar += padraoTexto(element);
+
+    }
+    divisaoResultados.innerHTML = textoPublicar;
+
+    return 0;
+
+}
+
+function ordernamento(listaNumeros) {
+    listaNumeros.sort(function (a, b) {
+        return a - b;
+    });
+}
+
+function padraoTexto(texto) {
+
+    return `<div class='apresentaDados'><p>${String(texto)}</p></div>`;
+}
+
+
 
 
